71179d0e6e0b99e16d03b7aa1cf84274a92bb9a7
김주희 <juhee1687@thinkware.co.kr>
2025-11-24 10:01:58 +0900

    fixed library path

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
 let package = Package(
     name: "FFmpegDecoder",
@@ -39,7 +40,7 @@ let package = Package(
                 .linkedLibrary("resolv"),
                 // FFmpeg 정적 라이브러리
                 .unsafeFlags([
-                    "-L\(String(describing: #filePath).dropLast(13))/libs",
+                    "-L\((#filePath as NSString).deletingLastPathComponent)/libs",
                     "-lavcodec",
                     "-lavfilter",
                     "-lavformat",
