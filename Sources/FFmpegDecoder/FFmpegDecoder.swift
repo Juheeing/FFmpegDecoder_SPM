a98dba1a1fcb730673a1e092e5a985dd4993b926
김주희 <juhee1687@thinkware.co.kr>
2025-11-26 14:57:59 +0900

    test openFile

--- Sources/FFmpegDecoder/FFmpegDecoder.swift
+++ Sources/FFmpegDecoder/FFmpegDecoder.swift
@@ -19,10 +19,108 @@ public protocol FFmpegDecoderDelegate: AnyObject {
     func decoder(_ decoder: FFmpegDecoder, didUpdateCurrentTime seconds: Int64, duration: Int64)
     func decoder(_ decoder: FFmpegDecoder, didReceiveVideoSize size: CGSize)
     func decoder(_ decoder: FFmpegDecoder, didReceiveSeeking isSeeking: Bool)
+    func decoder(_ decoder: FFmpegDecoder, didReceiveDecodedImage image: CIImage)
 }
 
 // MARK: - FFmpegDecoder
 
-public final class FFmpegDecoder {
+public final class FFmpegDecoder: @unchecked Sendable {
+    
+    public weak var delegate: FFmpegDecoderDelegate?
+    public private(set) var state: FFmpegDecoderState = .initialized {
+        didSet { delegate?.decoder(self, didChangeState: state) }
+    }
+    public private(set) var isSeeking = false {
+        didSet { delegate?.decoder(self, didReceiveSeeking: isSeeking) }
+    }
+    public private(set) var durationMs: Int64 = 0
+    public private(set) var currentTimeMs: Int64 = 0
 
+    // MARK: - FFmpeg C contexts (UnsafeMutablePointer)
+    private var formatCtx: UnsafeMutablePointer<AVFormatContext>?
+    private var videoCodecCtx: UnsafeMutablePointer<AVCodecContext>?
+    private var videoStreamIndex: Int32 = -1
+
+    // 디코딩 스레드
+    private let decodeQueue = DispatchQueue(label: "ffmpeg.decode.queue")
+    
+    public init() {
+        avformat_network_init()
+    }
+
+    // MARK: - Open File
+    public func open(url: String) {
+        state = .preparing
+
+        decodeQueue.async {
+            self.openFileInternal(url: url)
+        }
+    }
+
+    private func openFileInternal(url: String) {
+        var fmtCtx: UnsafeMutablePointer<AVFormatContext>? = avformat_alloc_context()
+
+        // 파일 열기
+        if avformat_open_input(&fmtCtx, url, nil, nil) != 0 {
+            state = .error
+            return
+        }
+
+        guard let formatCtx = fmtCtx else { return }
+        self.formatCtx = formatCtx
+
+        // 스트림 정보 읽기
+        avformat_find_stream_info(formatCtx, nil)
+
+        // 비디오 스트림 찾기
+        for i in 0 ..< Int(formatCtx.pointee.nb_streams) {
+            let stream = formatCtx.pointee.streams[i]
+            if stream?.pointee.codecpar.pointee.codec_type == AVMEDIA_TYPE_VIDEO {
+                videoStreamIndex = Int32(i)
+                break
+            }
+        }
+
+        if videoStreamIndex == -1 {
+            state = .error
+            return
+        }
+
+        // duration 계산
+        let duration = formatCtx.pointee.duration
+        let AV_TIME_BASE_Q = AVRational(num: 1, den: Int32(AV_TIME_BASE))
+        //let AV_TIME_BASE_Q = av_make_q(1, AV_TIME_BASE)
+
+        if duration > 0 {
+            durationMs = Int64(av_rescale_q(duration, AV_TIME_BASE_Q, AVRational(num: 1, den: 1000)))
+        }
+
+        // 비디오 디코더 준비
+        prepareVideoDecoder()
+
+        state = .readyToPlay
+    }
+    
+    private func prepareVideoDecoder() {
+        guard let formatCtx else { return }
+
+        let stream = formatCtx.pointee.streams[Int(videoStreamIndex)]
+        let codecPar = stream?.pointee.codecpar
+
+        guard let codec = avcodec_find_decoder(codecPar?.pointee.codec_id ?? AV_CODEC_ID_NONE) else {
+            state = .error
+            return
+        }
+
+        videoCodecCtx = avcodec_alloc_context3(codec)
+        guard let codecCtx = videoCodecCtx else { return }
+
+        avcodec_parameters_to_context(codecCtx, codecPar)
+        avcodec_open2(codecCtx, codec, nil)
+
+        // 해상도 delegate 전달
+        let size = CGSize(width: Int(codecCtx.pointee.width),
+                          height: Int(codecCtx.pointee.height))
+        delegate?.decoder(self, didReceiveVideoSize: size)
+    }
 }
