b6f27e09b92889591f36576a252402d189487ca4
김주희 <juhee1687@thinkware.co.kr>
2025-12-24 14:44:02 +0900

    Add missing code

--- Sources/FFmpegDecoder/FFmpegDecoder.h
+++ Sources/FFmpegDecoder/FFmpegDecoder.h
@@ -23,9 +23,7 @@
 @property (nonatomic, strong)AVAudioEngine *engine;
 @property (nonatomic, strong)AVAudioPlayerNode *player;
 
-- (void)startStreaming:(NSString *)url
-             ipAddress:(NSString *)ipAddress
-           withOptions:(NSDictionary<NSString *, NSString *> *)options;
+- (void) startStreaming:(NSString *)url withOptions:(NSDictionary<NSString *, NSString *> *)options;
 - (void) stopDecoding;
 - (void) pause;
 - (void) resume;
